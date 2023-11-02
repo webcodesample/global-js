300395ca100019fea6d1e7bef722931e78059e22
amitsriwastav <webcodesample@gmail.com>
2023-11-02 14:21:50 +0530

    new numbertoword converter ntw.php has added

--- modified by me/ntw.php
+++ modified by me/ntw.php
@@ -1,5 +1,34 @@
 <?php
-numberTowords(118000);
+numberTowords(9);
+echo "<br>";
+numberTowords(13);
+echo "<br>";
+numberTowords(57);
+echo "<br>";
+numberTowords(525);
+echo "<br>";
+numberTowords(4525);
+echo "<br>";
+numberTowords(64525);
+echo "<br>";
+numberTowords(894505);
+echo "<br>";
+numberTowords(804505);
+echo "<br>";
+numberTowords(804085);
+echo "<br>";
+numberTowords(804005);
+echo "<br>";
+numberTowords(505);
+echo "<br>";
+numberTowords(005);
+echo "<br>";
+numberTowords(0005);
+echo "<br>";
+numberTowords(804505.25);
+echo "<br>";
+numberTowords(9804505.25);
+
 
 function numberTowords($num)
 { 
@@ -24,8 +53,7 @@ $ones = array(
 18 => "eighteen", 
 19 => "nineteen" 
 ); 
-$tens = array( 
-
+$tens = array(
 2 => "twenty", 
 3 => "thirty", 
 4 => "forty", 
@@ -49,24 +77,149 @@ print_r($num_arr);
 echo "<br>";
 $wholenum = $num_arr[0]; 
 $decnum = $num_arr[1]; 
-$whole_arr = array_reverse(explode(",",$wholenum));
-print_r($whole_arr);
-echo "<br>";
-krsort($whole_arr);
+$whole_arr = explode(",",$wholenum);
 print_r($whole_arr);
 echo "<br>";
 
-$rettxt = ""; 
-foreach($whole_arr as $key => $i)
+$ntw_result = "";
+
+foreach($whole_arr as $key=>$value)
 { 
-if($i < 20)
+	echo "Array Length : ". count($whole_arr)."<br>";
+	if(count($whole_arr)>1)
+	{
+		if($key == count($whole_arr)-count($whole_arr))
+		{
+			echo "Array Index : ".$key."<br>";
+			$len = strlen($value);
+			echo "Key Value Length : ".$len;
+			echo "<br>";
+			for($len;$len>0;$len--)
+			{
+				if($len>2)
+				{
+					$ntw_result .= lakhOnly($value,$ones,$tens);
+					$value = substr($value,1,$len-1);
+				}
+				else
+				{
+					$ntw_result .= thousandOnly($value,$ones,$tens);
+					break;
+				}
+			}
+			//echo $len;
+			echo "<br>";
+		}
+
+		if($key == count($whole_arr)-(count($whole_arr)-1))
+		{
+			echo $key."<br>";
+			$len = strlen($value);
+			echo $len;
+			echo "<br>";
+			for($len;$len>0;$len--)
+			{
+				if($len>2)
+				{
+					$ntw_result .= hundredOnly($value,$ones,$tens);
+					$value = substr($value,1,$len-1);
+				}
+				else
+				{
+					$ntw_result .= twodigitOnly($value,$ones,$tens);
+					break;
+				}
+			}
+			//echo $len;
+			echo "<br>";
+		}
+	}
+
+	else 
+	{
+		echo $key."<br>";
+			$len = strlen($value);
+			echo $len;
+			echo "<br>";
+			for($len;$len>0;$len--)
+			{
+				if($len>2)
+				{
+					$ntw_result .= hundredOnly($value,$ones,$tens);
+					$value = substr($value,1,$len-1);
+				}
+				else
+				{
+					$ntw_result .= twodigitOnly($value,$ones,$tens);
+					break;
+				}
+			}
+	}
+
+}
+
+if($num_arr[1]>0)
+$decimal_value = " AND ".twodigitOnly($num_arr[1],$ones,$tens)." PAISE";
+else
+$decimal_value = " AND ZERO PAISE";
+
+echo "INR. ".strtoupper($ntw_result)." RUPEE".strtoupper($decimal_value)." ONLY";
+}
+
+function twodigitOnly($value,$ones,$tens)
+{	
+	if(substr($value,0,2)<20)
+	{
+		if(substr($value,0,1)>0)
+		return $ones[substr($value,0,2)];
+		else
+		return $ones[substr($value,1,1)];
+	}
+	else
+	{
+		return $tens[substr($value,0,1)]." ".$ones[substr($value,1,1)];
+	}
+}
+
+function hundredOnly($value,$ones,$tens)
+{
+	if(substr($value,0,1)>0)
+	return $ones[substr($value,0,1)]." Hundred ";
+}
+
+function thousandOnly($value,$ones,$tens)
+{
+	if(substr($value,0,2)<20)
+	{
+		if(substr($value,0,1)>0)
+		return $ones[substr($value,0,2)]." Thousand ";
+		else
+		return $ones[substr($value,1,1)]." Thousand ";
+	}
+	else
+	return $tens[substr($value,0,1)]." ".$ones[substr($value,1,1)]." Thousand ";
+}
+
+function lakhOnly($value,$ones,$tens)
+{
+	return $ones[substr($value,0,1)]." Lakh ";
+}
+
+
+
+
+
+
+
+
+/*if($i < 20)
 { 
-$rettxt .= $ones[$i]; 
+$rettxt .= $ones[$i];
 }
 elseif($i < 100 && $i>20)
 { 
 $rettxt .= $tens[substr($i,0,1)]; 
-$rettxt .= " ".$ones[substr($i,1,1)]; 
+$rettxt .= " ".$ones[substr($i,1,1)];
 }
 else
 {
@@ -93,5 +246,6 @@ $rettxt .= $tens[substr($decnum,0,1)];
 $rettxt .= " ".$ones[substr($decnum,1,1)]; 
 } 
 } 
-echo $rettxt; 
-}  ?>
+echo $rettxt;
+}*/
+?>
